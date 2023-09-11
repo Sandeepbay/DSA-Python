c84e56bb826ffbe2b14a2b2ee7d40cd7d511c4a5
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-09-11 23:29:44 +0530

    Update main.py

--- LinkedList/Doubly Linked List/main.py
+++ LinkedList/Doubly Linked List/main.py
@@ -1,31 +1,29 @@
-class Node:
-    def __init__(self,value):
-        self.value = value
-        self.next = None
-        self.prev = None
+# class Node:
+#     def __init__(self,value):
+#         self.value = value
+#         self.next = None
+#         self.prev = None
 
-class DoubleLinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+# class DoubleLinkedList:
+#     def __init__(self):
+#         self.head = None
+#         self.tail = None
 
-    def __iter__(self):
-        node = self.head
-        while node:
-            yield node
-            node = node.next
+#     def __iter__(self):
+#         node = self.head
+#         while node:
+#             yield node
+#             node = node.next
 
-    def create(self,value):
-        node = Node(value)
-        node.next = None
-        node.prev = None
-        self.head = node
-        self.tail = node
-        print(node)
-        return "New Node has been created"
+#     def create(self,value):
+#         node = Node(value)
+#         node.next = None
+#         node.prev = None
+#         self.head = node
+#         self.tail = node
+#         print(node)
+#         return "New Node has been created"
 
-new_Double_Linked_List = DoubleLinkedList()
-print(new_Double_Linked_List.create(5))
-print(node.value for node in new_Double_Linked_List)
-
-
+# new_Double_Linked_List = DoubleLinkedList()
+# print(new_Double_Linked_List.create(5))
+# print(node.value for node in new_Double_Linked_List)
