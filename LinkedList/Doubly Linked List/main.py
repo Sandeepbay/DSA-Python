6ed3c96a5a29086562531910012fa7598566ddcf
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-09-13 18:18:58 +0530

    Documentation

--- LinkedList/Doubly Linked List/main.py
+++ LinkedList/Doubly Linked List/main.py
@@ -1,14 +1,17 @@
+#Creating a node class
 class Node:
     def __init__(self,value):
         self.value = value
         self.next = None
         self.prev = None
 
+#Creating Doubly Linked List Class
 class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
 
+    #Method to iterate over the Linked List
     def __iter__(self):
         node = self.head
         while node is not None:
@@ -24,6 +27,7 @@ class DoublyLinkedList:
         self.tail = node
         return "The node has been created succesfully"
     
+    #Method to print the Linked List
     def print_forward(self):
         current = self.head
         while current:
@@ -31,6 +35,7 @@ class DoublyLinkedList:
             current = current.next
         print("None")
 
+    #Method to print the Linked List backwards
     # def print_backward(self):
     #     current = self.tail
     #     while current:
@@ -38,21 +43,25 @@ class DoublyLinkedList:
     #         current = current.prev
     #     print("None")
     
+    #Method to add Node inside the Linked List
     def addNode(self,value,position):
         if self.head is None:
             print("Node cannot be inserted")
         else:
             new_node = Node(value)
+            #Method to add Node inside the Linked List in the beginning
             if position == 0:
                 new_node.next = self.head
                 self.head.prev = new_node
                 new_node.prev = None
                 self.head = new_node
+            #Method to add Node inside the Linked List in the end
             elif position == 1:
                 new_node.next = None
                 new_node.prev = self.tail
                 self.tail.next = new_node
                 self.tail = new_node
+            #Method to add Node inside the Linked List anywhere
             else:
                 temp_node = self.head
                 index = 0
@@ -64,18 +73,21 @@ class DoublyLinkedList:
                 new_node.next.prev = new_node
                 temp_node.next = new_node
 
+    #Method to traverse inside the Linked List
     def traversal(self):
         temp_node = self.head
         while temp_node is not None:
             print(temp_node.value)
             temp_node = temp_node.next
 
+    #Method to reverse traverse inside the Linked List
     def reverseTraversal(self):
         temp_node = self.tail
         while temp_node is not None:
             print(temp_node.value)
             temp_node = temp_node.prev
 
+    #Method to find an element inside the Linked List
     def search(self,target):
         temp_node = self.head
         index = 0
@@ -86,9 +98,11 @@ class DoublyLinkedList:
             index += 1
         return "Element has not been found"
 
+    #Method to delete Node inside the Linked List
     def deleteNode(self,location):
         if self.head is None:
             return "No elements present"
+        #Method to delete Node inside the Linked List in the beginning
         elif location == 0:
             if self.head == self.tail:
                 self.head = None
@@ -96,6 +110,7 @@ class DoublyLinkedList:
             else:
                 self.head = self.head.next
                 self.head.prev = None
+        #Method to delete Node inside the Linked List in tehe end
         elif location == 1:
             if self.head == self.tail:
                 self.head = None
@@ -103,6 +118,7 @@ class DoublyLinkedList:
             else:
                 self.tail = self.tail.prev
                 self.tail.next = None
+        #Method to delete Node inside the Linked List anywhere        
         else:
             temp_node = self.head
             index = 0
@@ -113,6 +129,7 @@ class DoublyLinkedList:
             temp_node.next.prev = temp_node
         print("The node has been succesfully deleted")
 
+    #Method to delete all the Nodes inside the Linked List
     def DeleteAllNode(self):
         temp_node = self.head
         while temp_node is not None:
@@ -122,7 +139,7 @@ class DoublyLinkedList:
         self.tail = None
         print("All Nodes has been deleted")
 
-
+#All operations carried out
 DoublyLL = DoublyLinkedList()
 DoublyLL.create(5)
 DoublyLL.addNode(0,0)
