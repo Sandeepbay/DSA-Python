aa15ac929a5c964fd4f33ff671a865307c4756b2
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-09-13 17:51:46 +0530

    Complete Doubly Linked List

--- LinkedList/Doubly Linked List/main.py
+++ LinkedList/Doubly Linked List/main.py
@@ -76,16 +76,66 @@ class DoublyLinkedList:
             print(temp_node.value)
             temp_node = temp_node.prev
 
+    def search(self,target):
+        temp_node = self.head
+        index = 0
+        while temp_node is not None:
+            if temp_node.value == target:
+                return f"Element has been found at index {index}"
+            temp_node = temp_node.next
+            index += 1
+        return "Element has not been found"
+
+    def deleteNode(self,location):
+        if self.head is None:
+            return "No elements present"
+        elif location == 0:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.head = self.head.next
+                self.head.prev = None
+        elif location == 1:
+            if self.head == self.tail:
+                self.head = None
+                self.tail = None
+            else:
+                self.tail = self.tail.prev
+                self.tail.next = None
+        else:
+            temp_node = self.head
+            index = 0
+            while index < location - 1:
+                temp_node = temp_node.next
+                index +=1
+            temp_node.next = temp_node.next.next
+            temp_node.next.prev = temp_node
+        print("The node has been succesfully deleted")
+
+    def DeleteAllNode(self):
+        temp_node = self.head
+        while temp_node is not None:
+            temp_node.prev = None
+            temp_node = temp_node.next
+        self.head = None
+        self.tail = None
+        print("All Nodes has been deleted")
+
+
 DoublyLL = DoublyLinkedList()
 DoublyLL.create(5)
 DoublyLL.addNode(0,0)
 DoublyLL.addNode(10,1)
 DoublyLL.addNode(7.5,2)
-#DoublyLL.traversal()
-#DoublyLL.reverseTraversal()
-
 print("Forward:")
 DoublyLL.print_forward()
+#DoublyLL.DeleteAllNode()
+#DoublyLL.print_forward()
+#DoublyLL.traversal()
+#DoublyLL.reverseTraversal()
+#print(DoublyLL.search(7.5))
+#DoublyLL.deleteNode(2)
 
 # print("Backward:")
 # DoublyLL.print_backward()
