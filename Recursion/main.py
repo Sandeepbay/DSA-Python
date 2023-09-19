5dcccf644c2211360ea80839a78ee9af7d3df11e
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-09-19 23:59:21 +0530

    Recursion

---
+++ Recursion/main.py
@@ -0,0 +1,3 @@
+# Performing the same operation multiple times with different inputs
+# In every step we try smaller inputs to make the problem smaller
+# Base condition is needed to stop the recursion , otherwise infinite loop will occur.
