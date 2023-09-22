5997146a38c2284cc9b14a64e78f78ad2baf6d94
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-09-22 13:33:59 +0530

    Tree

---
+++ Tree/main.py
@@ -0,0 +1,15 @@
+# A Tree is a non - Linear data structure with heirarchial relationships between its elements without any cycle.
+# Tree properties:
+# 1. Represent hierarchal data
+# 2. Each Node has two components : data and a link to its sub category
+# 3. Base Category ans sub categories under it. 
+
+#Tree Terminology - 
+#1. Root - Top Node without parent
+#2. Edge - A link between parent and child
+#3. Leaf - A Node which does not have children
+#4. Sibling - Children of same parent
+#5. Ancestor - parent , grandparent and great grandparent of a node
+#6. Depth of the node - a length of the path from root to the node
+#7. Height of the node - a length of the path from the node to the deepest node
+#8. Height of the tree - Height of root node
