6a8a0305ba5c1652536098bdcaa2b1bf3dd8b012
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-10-09 23:39:31 +0530

    Graph Algorithm

---
+++ Graph/main.py
@@ -0,0 +1,15 @@
+# Graph consists of a finite set of vertices or nodes and a set of edges which connect a pair of nodes.
+
+# Vertices - Vertices are the nodes of the graph.
+#Edge - The Edge is the line that connect pair of vertices.
+#Unweighted Graph - A Graph which does not have a weight associated with any graph.
+#Weighted Graph - A Graph which has a weight associated with any edge.
+#Undirected Graph - In case the edges of the graph do not have a direction associated with them.
+#Directed Graph - If the edges has a direction associated with them.
+#Cyclic Graph - A Graph which has at least one loop.
+# Acyclic Graph - A Graph with no loop
+# Tree - It is a special case of directed Acyclic graph.
+
+# We can represent Graph in two ways - 
+# 1. Adjacency Matrix - It is stored in a 2D Matrix.
+# 2. Adjacency List - It is stored in the form of a Linked List , where the nodes are stored in the form of the list , while the edges are stored to the respective nodes.
