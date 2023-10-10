624728c2dd15de6b7ab4f47907f5ae0731b832ef
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-10-10 19:08:26 +0530

    More Graph

--- Graph/main.py
+++ Graph/main.py
@@ -13,3 +13,79 @@
 # We can represent Graph in two ways - 
 # 1. Adjacency Matrix - It is stored in a 2D Matrix.
 # 2. Adjacency List - It is stored in the form of a Linked List , where the nodes are stored in the form of the list , while the edges are stored to the respective nodes.
+
+# OLd form of representing a Graph
+# class Graph:
+#     def __init__(self , gDict=None):
+#         if gDict == None:
+#             gDict = {}
+#         self.gDict = gDict
+
+#     def addEdge(self,vertex,edge):
+#         self.gDict[vertex].append(edge)
+
+# customGraph = {
+#     "a" : ["b" , "c"],
+#     "b" : ["d" , "e"],
+#     "c" : ["a" , "e"],
+#     "d" : ["b" , "f" , "e"],
+#     "e" : ["b" , "d" , "f"],
+#     "f" : ["d" , "e"],
+# }
+
+# newGraph = Graph(customGraph)
+# newGraph.addEdge("b" , "a")
+# print(newGraph.gDict)
+
+# New form of representing a graph
+class Graph:
+    def __init__(self):
+        self.gList = {}
+
+    def addVertex(self , vertex):
+        if vertex not in self.gList.keys():
+            self.gList[vertex] = []
+            return True
+        return False
+
+    def printGraph(self):
+        for vertex in self.gList:
+            print(vertex,":",self.gList[vertex])
+
+    def addEdge(self,vertex,edge):
+        if vertex in self.gList.keys():
+            self.gList[vertex].append(edge)
+            return True
+        return False
+
+    def removeEdge(self , vertex , edge):
+        if vertex in self.gList.keys():
+            self.gList[vertex].remove(edge)
+            return True
+        return False
+    
+    def removeVertex(self,vertex):
+        if vertex in self.gList.keys():
+            for other_vertex in self.gList[vertex]:
+                self.gList[other_vertex].remove(vertex)
+            del self.gList[vertex]
+            return True
+        return False
+
+
+customList = Graph()
+customList.addVertex("A")
+customList.addVertex("B")
+customList.addVertex("C")
+customList.addEdge("A" , "B")
+customList.addEdge("A" , "C")
+customList.addEdge("B" , "A")
+customList.addEdge("B" , "C")
+customList.addEdge("C" , "B")
+customList.addEdge("C" , "A")
+customList.printGraph()
+print("After removing the Edge")
+# customList.removeEdge("A" , "B")
+#customList.removeVertex("B")
+customList.printGraph()
+
